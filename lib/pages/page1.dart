e723d802071f344c91838fbc218d627b6613bf57
Uriel Aispuro <urielalexis64@gmail.com>
2021-07-31 15:10:34 -0600

    add example

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:route_transitions/helpers/route_transitions.dart';
+import 'package:route_transitions/helpers/uas_transition.dart';
 import 'package:route_transitions/pages/page2.dart';
 
 class Page1Page extends StatelessWidget {
@@ -12,14 +12,45 @@ class Page1Page extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       body: Center(
-        child: MaterialButton(
-          onPressed: () {
-            RouteTransitions(context: context, child: Page2Page());
-          },
-          child: Text('Go to Page2'),
-          color: Colors.white,
+        child: ShakeTransition(
+          child: MaterialButton(
+            onPressed: () {
+              UasTransition(
+                  context: context,
+                  child: Page2Page(),
+                  animation: UasAnimation.rotateFromBottomRight,
+                  curves: Curves.ease);
+            },
+            child: Text('Go to Page2'),
+            color: Colors.white,
+          ),
         ),
       ),
+    );
+  }
+}
+
+class ShakeTransition extends StatelessWidget {
+  final Duration duration;
+  final Widget child;
+  final double offset;
+
+  const ShakeTransition(
+      {this.duration = const Duration(milliseconds: 500),
+      this.offset = 100.0,
+      required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 1.0, end: 0.0),
+      duration: duration,
+      curve: Curves.elasticOut,
+      child: child,
+      builder: (context, value, child) {
+        return Transform.translate(
+            offset: Offset(value * offset, 0.0), child: child);
+      },
     );
   }
 }
